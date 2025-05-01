98f7f1afa9c39a661ee143bd419fea536217e251
shota shrestha <shota.shrestha@gmail.com>
2025-05-01 16:49:11 +0900

    Fix: remove 404 text and correct Button export

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -49,4 +49,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 )
 Button.displayName = "Button"
 
-export { Button, buttonVariants }404: Not Found
+export { Button, buttonVariants };
