ea176cbe3323dc0a5b47a6e631f12cbc7bdf24c9
shota shrestha <shota.shrestha@gmail.com>
2025-05-02 15:56:57 +0900

    Fix: avatar image sizing in timeline

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -111,7 +111,7 @@ export default function Timeline() {
                   <img
                     src={userInfo.avatarUrl}
                     alt="avatar"
-                    className="w-6 h-6 rounded-full object-cover"
+                    className="w-8 h-8 rounded-full object-cover"
                   />
                   {userInfo.displayName}
                 </div>
