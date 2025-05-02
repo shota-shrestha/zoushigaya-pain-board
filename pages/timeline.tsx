a9d0ad53cd920dc7f8bcff10c4df908133a6c94d
shota shrestha <shota.shrestha@gmail.com>
2025-05-02 16:12:40 +0900

    Fix: avatar size enforced in timeline

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -111,7 +111,8 @@ export default function Timeline() {
                   <img
                     src={userInfo.avatarUrl}
                     alt="avatar"
-                    className="w-8 h-8 rounded-full object-cover"
+                    className="rounded-full object-cover inline-block shrink-0"
+                    style={{ width: "32px", height: "32px" }}
                   />
                   {userInfo.displayName}
                 </div>
